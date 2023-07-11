f5fc2c19cc275cab033f830ef64dcd330849d174
shubham1upadhyay <shubhamup47@gmail.com>
2023-07-12 01:31:05 +0530

    code added

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,17 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[val, setVal] = useState("");
+
+  function display(e){
+    return setVal(e.target.value);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <label>Enter your name:</label><br/>
+        <input type="text" onChange={display} value={val} />
+        <p>{"Hello " + val + "!"}</p>
     </div>
   )
 }
